9e9c2e0d25a6870c54bea36a55cb653f10ae0b34
praveenpkg8 <praveenpkg8@gmail.com>
2019-02-01 11:28:02 +0530

    Added utils package for handling exceptions

---
+++ util/exceptions.py
@@ -0,0 +1,32 @@
+class NoteNotFoundException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class RecordNotFoundException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class DataBaseSessionException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class NoteDeletedException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+class UserNameNotFoundException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+class ContentNotFoundException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
