547dacc7e585e81ad16f7afe4cbda6e8434284dc
praveenpkg8 <praveenpkg8@gmail.com>
2019-02-01 15:20:08 +0530

    added try catch for save note handling key error

--- modules/notes/notes.py
+++ modules/notes/notes.py
@@ -1,14 +1,12 @@
 import json
-
 from flask import Blueprint, request
-
 from modules.notes.composed import fetch_all_notes
 from models.todo import (Notes, NoteNotFoundException,
                          logger)
 from modules.notes.composed import construct_response_message
 from modules.notes.parser import NoteParser
 from util.exceptions import (NoteDeletedException, DataBaseSessionException,
-                             UserNameNotFoundException, ContentNotFoundException)
+                             RecordNotFoundException)
 from status import status
 
 bp = Blueprint('notes', __name__, url_prefix='/todo')
@@ -16,9 +14,15 @@ bp = Blueprint('notes', __name__, url_prefix='/todo')
 
 @bp.route('/notes', methods=['GET'])
 def view():
-    data = fetch_all_notes()
-    message = construct_response_message(notes= data)
-    return json.dumps(message), status.HTTP_200_OK
+    try:
+        data = fetch_all_notes()
+        message = construct_response_message(notes=data)
+        return json.dumps(message), status.HTTP_200_OK
+
+    except RecordNotFoundException as e:
+        logger.error(msg=e.error_message)
+        message = construct_response_message(error_message=e.error_message)
+        return json.dumps(message), status.HTTP_404_NOT_FOUND
 
 
 @bp.route('/notes/<int:note_id>', methods=['GET'])
@@ -38,9 +42,7 @@ def view_one(note_id):
 @bp.route('/notes', methods=['PUT'])
 def save_note():
     try:
-        note = request.get_json()
-        final_content = NoteParser.note_parse(note)
-        Notes.create_note(final_content)
+        NoteParser.note_parse()
         message = construct_response_message(message='Note created successfully')
         return json.dumps(message), status.HTTP_201_CREATED
 
@@ -49,15 +51,12 @@ def save_note():
         message = construct_response_message(error_message=e.error_message)
         return json.dumps(message), status.HTTP_500_INTERNAL_SERVER_ERROR
 
-    except UserNameNotFoundException as e:
-        logger.error(msg=e.error_message)
-        message = construct_response_message(error_message=e.error_message)
-        return json.dumps(message), status.HTTP_500_INTERNAL_SERVER_ERROR
 
-    except ContentNotFoundException as e:
-        logger.error(msg=e.error_message)
-        message = construct_response_message(error_message=e.error_message)
-        return json.dumps(message), status.HTTP_500_INTERNAL_SERVER_ERROR
+    except KeyError as err:
+        error_message = "Key error missing"
+        logger.error(msg=error_message)
+        message = construct_response_message(error_message=error_message + str(err))
+        return json.dumps(message), status.HTTP_404_NOT_FOUND
 
 
 @bp.route('/notes/<int:note_id>', methods=['PUT'])
@@ -99,5 +98,3 @@ def delete_note(note_id):
         logger.error(msg=e.error_message)
         message = construct_response_message(error_message=e.error_message)
         return json.dumps(message), status.HTTP_500_INTERNAL_SERVER_ERROR
-
-
