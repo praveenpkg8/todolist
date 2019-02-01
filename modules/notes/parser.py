4cb379252bf13eda98dbbdcb303c1364f7e1f485
praveenpkg8 <praveenpkg8@gmail.com>
2019-02-01 15:21:11 +0530

    added note parse function

--- modules/notes/parser.py
+++ modules/notes/parser.py
@@ -1,18 +1,22 @@
 from typing import List, Dict
 from models.todo import Notes
-from util.exceptions import UserNameNotFoundException, ContentNotFoundException
+from flask import request
 
 
 class NoteParser:
     @staticmethod
-    def note_parse(note: Dict) -> object:
-        created_by = note['created_by']
-        content = note['content']
-        Notes(created_by, content)
-        if created_by is None:
-            raise UserNameNotFoundException("No Name Found")
-        elif content is None:
-            raise ContentNotFoundException("No content Found")
-        else:
-            return Notes
+    def note_parse():
+        try:
+            request_note = request.get_json()
+            created_by = request_note['created_by']
+            content = request_note['content']
+            final_note = Notes(created_by, content)
+            Notes.create_note(final_note)
+
+        except KeyError as err:
+            raise err
+
+
+
+
 
